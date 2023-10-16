7d3510b408d90a2acaaf061812601e8e59cba1db
Romumrn <romuald.mrn@outlook.fr>
2023-10-16 16:09:30 +0200

    add argparse

--- run_martini_AI.py
+++ run_martini_AI.py
@@ -1,46 +1,39 @@
 # -*- coding: utf-8 -*-
-
-from transformers import T5EncoderModel, T5Tokenizer
+import argparse
 import torch
-import h5py
-import numpy as np
 import pandas as pd
-import time
-import sys
-import os
-import wget
+from martini_AI import create_folder_and_download_files, get_T5_model, get_prediction
 from urllib.request import Request, urlopen
-import torch
-from martini_AI import create_folder_and_download_files , get_T5_model, get_prediction
-torch.cuda.empty_cache()
+from transformers import T5EncoderModel
+from tqdm import tqdm
+import wget
+import h5py
 
-#@title Read in file in fasta format. { display-mode: "form" }
-def read_fasta( fasta_path, split_char="!", id_field=0):
+# Check whether GPU is available.
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+# Function to read a FASTA file and extract sequences.
+def read_fasta(fasta_path, split_char="!", id_field=0):
     '''
-        Reads in fasta file containing multiple sequences.
-        Split_char and id_field allow to control identifier extraction from header.
-        E.g.: set split_char="|" and id_field=1 for SwissProt/UniProt Headers.
-        Returns dictionary holding multiple sequences or only single
-        sequence, depending on input file.
+    Reads in a FASTA file containing multiple sequences. 
+    Allows control over identifier extraction from the header.
+    Returns a dictionary holding sequences.
     '''
 
-    seqs = dict()
-    with open( fasta_path, 'r' ) as fasta_f:
-        for line in fasta_f:
-            # get uniprot ID from header and create new entry
+    sequences = dict()
+    with open(fasta_path, 'r') as fasta_file:
+        for line in fasta_file:
             if line.startswith('>'):
                 uniprot_id = line.replace('>', '').strip().split(split_char)[id_field]
-                # replace tokens that are mis-interpreted when loading h5
-                uniprot_id = uniprot_id.replace("/","_").replace(".","_")
-                seqs[ uniprot_id ] = ''
+                uniprot_id = uniprot_id.replace("/", "_").replace(".", "_")
+                sequences[uniprot_id] = ''
             else:
-                # repl. all whie-space chars and join seqs spanning multiple lines, drop gaps and cast to upper-case
-                seq= ''.join( line.split() ).upper().replace("-","")
-                # repl. all non-standard AAs and map them to unknown/X
-                seq = seq.replace('U','X').replace('Z','X').replace('O','X')
-                seqs[ uniprot_id ] += seq
-    return seqs
+                seq = ''.join(line.split()).upper().replace("-", "")
+                seq = seq.replace('U', 'X').replace('Z', 'X').replace('O', 'X')
+                sequences[uniprot_id] += seq
+    return sequences
 
+# Function to check if a file is in FASTA format.
 def is_fasta(file_path):
     try:
         with open(file_path, 'r') as file:
@@ -49,59 +42,50 @@ def is_fasta(file_path):
     except FileNotFoundError:
         return False
 
-# Load example fasta.
-if sys.argv[1] :
-    if is_fasta(sys.argv[1]):
-        seqs  =read_fasta(sys.argv[1])
+# Parse command-line arguments.
+parser = argparse.ArgumentParser(description="Martini Dihedrals AI-Powered Sequence Analysis")
+parser.add_argument("--sequence", "-s", help="Input amino acid sequence for analysis.")
+parser.add_argument("--sequence_id", "-id", help="Sequence identifier (optional). Default: 'seq000'", default="seq000")
+parser.add_argument("--fasta", "-f", help="FASTA file containing an amino acid sequence (only one sequence is processed at the moment).")
+args = parser.parse_args()
+
+# Check the arguments and take appropriate actions.
+if args.fasta:
+    if not is_fasta(args.fasta):
+        print("Error: The provided file does not appear to be a valid FASTA file.")
+        quit()
+    else:
+        seqs = read_fasta(args.fasta)
         id = list(seqs.keys())[0]
         seq = seqs[id]
-        print( f"Fasta sequence loaded {id}")
-    else:
-        seq = sys.argv[1]
-        if sys.argv[2]:
-            id = sys.argv[2]
-        else:
-            id = "seq000"
-    
+        print(f"Fasta sequence loaded with identifier: {id}")
+elif args.sequence:
+    seq = args.sequence
+    id = args.sequence_id
 else:
-    print( "Please provide a sequence")
-    exit
+    print("Please provide input data (amino acid sequence or FASTA file) for analysis.")
+    quit()
 
-
-#@title Import dependencies and check whether GPU is available. { display-mode: "form" }
-
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
+# Create a folder and download required files.
 create_folder_and_download_files()
-#@title Load the checkpoint for secondary structure prediction. { display-mode: "form" }
+
+# Load the checkpoint for secondary structure prediction.
 preloaded_model = get_T5_model()
 
-
-
-# Compute embeddings and/or secondary structure predictions
-print( "get prediction")
-
+# Compute embeddings and/or secondary structure predictions.
+print("Getting prediction...")
 result = get_prediction(preloaded_model, seq, id)
 
-
-
-# Mapping of DSSP8 values to secondary structure classes
-# 310 helix (G), α-helix (H), π-helix (I), β-strand (E),
-# bridge (B), turn (T), bend (S), and others (C).
+# Mapping of DSSP8 values to secondary structure classes.
 dssp8 = ["G", "H", "I", "E", "B", "T", "S", "C"]
 
-# convert array into dataframe
-# DF = pd.DataFrame(result)
-# DF.columns = ["AA"] + dssp8
-# DF.pop(DF.columns[0])
-# max_index = DF.astype(float).idxmax(axis=1)
-# list_predicted = max_index.tolist()
+# Convert the array into a DataFrame and save the results to a CSV file.
 DF = pd.DataFrame(result)
 DF.columns = ["AA"] + dssp8
 
-filename=id+"_prediction.csv"
-DF.to_csv(filename, index=False)  
+filename = f"{id}_prediction.csv"
+DF.to_csv(filename, index=False)
 
-print( DF.describe() )
-
-print( f"Save in {filename}")
+print("Statistics for secondary structure prediction:")
+print(DF.describe())
+print(f"Results saved in {filename}")
